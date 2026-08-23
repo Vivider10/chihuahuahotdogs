0bc6377f527ae9e382594d4b95def15079b1b669
Vivider <118007939+Vivider10@users.noreply.github.com>
2026-08-22 23:31:49 -0400

    Load first responder discount styling on POS

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import '../discount.css'
 import PointOfSale from '../components/PointOfSale'
 
 export const Route = createFileRoute('/')({
